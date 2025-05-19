e74106d3ceff59e3dc91aa74e9720b47acd488a3
Soron248 <iftekhar.soron@gmail.com>
2025-05-19 16:47:11 +0600

    Add API response handling and category management
    Implemented a new `ApiResponse` class for standardized API responses. Added `CategoryController` and `Category` model for category management. Updated `WeatherForecastController` and `WeatherForecast` class. Configured `Program.cs`, `launchSettings.json`, and `appsettings.json`. Updated project file with necessary package references and created a `.gitignore` to exclude build artifacts. Added an HTTP request file for testing API endpoints.

---
+++ Controllers/ApiResponse.cs
@@ -0,0 +1,38 @@
+﻿namespace testapiproject.Controllers
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+
+        public int Status { get; set; }
+
+        public T? Data { get; set; }
+
+        public List<string>? Errors { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        //Constructor for successfull responce
+        private ApiResponse(bool success, string message, T? data, List<string> errors, int statusCode)
+        {
+            Success = success;
+            Message = message;
+            Status = statusCode;
+            Data = data;
+            Errors = errors;
+            TimeStamp = DateTime.Now;
+        }
+
+        //Static method for successfull responce
+        public static ApiResponse<T> SuccessResponse(T data, int statusCode, string message = "")
+        {
+            return new ApiResponse<T>(true, message, data, null, statusCode);
+        }
+
+        //Constructor for error responce
+
+    }
+
+}
